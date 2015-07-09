3771a1e562ed355d0e6c675bdcee3e6e8033e53d
Chris Grandin <cgrandin@shaw.ca>
2015-07-09 10:19:23 -0700

    Add relative biomass table funcion call and fix the gui so that there is only one check box for writing the table to disk, not one for each table.

--- iscam-gui.r
+++ iscam-gui.r
@@ -367,8 +367,9 @@ iscam <- function(reloadScenarios      = FALSE,
   # Number of decimal places to report in tables
   ndigits <- val$numTableDigits
 
-  # For returning XTables for latex
+  # For returning XTables for latex, and whether or not to save the table to file
   retxtable <- val$returnXTable
+  savetable <- val$writeTable
 
   # For catch tables
   startYearCatch <- val$entryCatchStart
@@ -491,39 +492,33 @@ iscam <- function(reloadScenarios      = FALSE,
            setWinVal(c(entryIndex=nextGroup))
          },
          "paramEstTable" = {
-           savetable <- val$writeParamEstTable
            makeTable(s,1,savetable,"ParamEst",ci,multiple=FALSE,sensGroup=sgr,burnthin=burnthin, digits=ndigits, retxtable=retxtable)
          },
          "refPointsTable" = {
-           savetable <- val$writeRefPointsTable
            makeTable(s,2,savetable,"RefPoints",ci,multiple=FALSE,sensGroup=sgr,burnthin=burnthin, digits=ndigits, retxtable=retxtable)
          },
          "biomassTable" = {
-           savetable <- val$writeBiomassTable
            makeTable(s,3,savetable,"Biomass",ci,multiple=FALSE,sensGroup=sgr,burnthin=burnthin, digits=ndigits, retxtable=retxtable)
          },
+         "depletionTable" = {
+           makeTable(s,9,savetable,"RelBiomass",ci,multiple=FALSE,sensGroup=sgr,burnthin=burnthin, digits=ndigits, retxtable=retxtable)
+         },
          "recruitmentTable" = {
-           savetable <- val$writeRecruitmentTable
            makeTable(s,4,savetable,"Recruitment",ci,multiple=FALSE,sensGroup=sgr,burnthin=burnthin, digits=ndigits, retxtable=retxtable)
          },
          "fTable" = {
-           savetable <- val$writeFTable
            makeTable(s,5,savetable,"F",ci,multiple=FALSE,sensGroup=sgr,burnthin=burnthin, digits=ndigits, retxtable=retxtable)
          },
          "uTable" = {
-           savetable <- val$writeUTable
            makeTable(s,6,savetable,"U",ci,multiple=FALSE,sensGroup=sgr,burnthin=burnthin, digits=ndigits, retxtable=retxtable)
          },
          "decisionTable" = {
-           savetable <- val$writeDecisionTable
            makeTable(s,7,savetable,"Decision",ci,multiple=FALSE,sensGroup=sgr,burnthin=burnthin, digits=ndigits, retxtable=retxtable)
          },
          "catchTable" = {
-           savetable <- val$writeCatchTable
            makeTable(s,10,savetable,"Catch", digits=ndigits, retxtable=retxtable, from=startYearCatch, to=endYearCatch)
          },
          "indicesTable" = {
-           savetable <- val$writeIndicesTable
            makeTable(s,8,savetable,"Indices",ci,multiple=FALSE,sensGroup=sgr,burnthin=burnthin, digits=ndigits, retxtable=retxtable)
          },
          "runCurrScenario" = {
